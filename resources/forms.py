8a71e698b221216f3c0e56c95de22d2ede44c80f
Jon Lee <39420293+4ka0@users.noreply.github.com>
2021-11-02 21:34:28 +0900

    Upload glossary validation

--- resources/forms.py
+++ resources/forms.py
@@ -96,3 +96,41 @@ class GlossaryUploadForm(forms.ModelForm):
     class Meta:
         model = GlossaryUploadFile
         fields = ("file_name", "glossary_title")
+
+    def clean(self):
+        '''
+        Overwritten to handle validation regarding whether the content of the uploaded file
+        is to be added to an existing glossary or a new glossary.
+        '''
+        cleaned_data = super().clean()
+        existing_glossary = cleaned_data.get('existing_glossary')
+        new_glossary = cleaned_data.get('glossary_title')
+
+        # If both fields have been entered, output error
+        if existing_glossary and new_glossary:
+            existing_glossary_msg = "Please select an existing glossary ..."
+            new_glossary_msg = "... or create a new glossary, not both."
+            self.add_error('existing_glossary', existing_glossary_msg)
+            self.add_error('glossary_title', new_glossary_msg)
+
+        # If neither of the fields have been entered, output error
+        if not (existing_glossary or new_glossary):
+            existing_glossary_msg = "Please select an existing glossary ..."
+            new_glossary_msg = "... or create a new glossary."
+            self.add_error('existing_glossary', existing_glossary_msg)
+            self.add_error('glossary_title', new_glossary_msg)
+
+        # If new term is to be added to a new glossary
+        if not existing_glossary and new_glossary:
+            # If input title for new glossary already exists, output error
+            if Glossary.objects.filter(title__iexact=new_glossary).exists():
+                msg = 'A glossary with that title already exists.'
+                self.add_error('glossary_title', msg)
+            else:
+                # Create new Glossary instance having title from new_glossary
+                create_glossary = Glossary(title=new_glossary)
+                create_glossary.save()
+                # Add new glossary object to form data (immutable so have to use copy() here)
+                cleaned_data = self.data.copy()
+                cleaned_data['glossary'] = create_glossary
+                return cleaned_data
