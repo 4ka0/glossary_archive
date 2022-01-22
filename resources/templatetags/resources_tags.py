6a07f0700859cce082141baade23d4c2ba30401a
Jon Lee <39420293+4ka0@users.noreply.github.com>
2022-01-22 20:16:34 +0900

    Fixed template filter bug

--- resources/templatetags/resources_tags.py
+++ resources/templatetags/resources_tags.py
@@ -8,6 +8,9 @@ register = template.Library()
 
 @register.filter
 def highlight_query(text, query):
-    highlighted = re.sub('(?i)(%s)' % (re.escape(query)),
-                         '<span class="highlight_query">\\1</span>', text)
-    return mark_safe(highlighted)
+    # Only highlight query if there actually is one.
+    if query != "":
+        highlighted = re.sub('(?i)(%s)' % (re.escape(query)),
+                             '<span class="highlight_query">\\1</span>', text)
+        return mark_safe(highlighted)
+    return mark_safe(text)
