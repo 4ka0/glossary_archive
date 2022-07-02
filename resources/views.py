77834d27df21c39cd64f46e065464f16c38c3cc9
Jon Lee <39420293+4ka0@users.noreply.github.com>
2022-07-02 19:57:58 +0900

    Cancel button added to update view

--- resources/views.py
+++ resources/views.py
@@ -147,6 +147,15 @@ class EntryUpdateView(LoginRequiredMixin, UpdateView):
         previous_url = self.request.GET.get('previous_url')
         return HttpResponseRedirect(previous_url)
 
+    def post(self, request, *args, **kwargs):
+        """Over-ridden to check if the cancel button has been pressed
+           instead of the submit button on the update form."""
+        if "cancel" in request.POST:
+            previous_url = self.request.GET.get('previous_url')
+            return HttpResponseRedirect(previous_url)
+        else:
+            return super(EntryUpdateView, self).post(request, *args, **kwargs)
+
 
 class EntryDeleteView(LoginRequiredMixin, DeleteView):
     model = Entry
