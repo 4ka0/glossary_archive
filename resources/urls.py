40f0fbc9f8dd611e799cedcaa8e19e2a770beec1
Jon Lee <39420293+4ka0@users.noreply.github.com>
2022-07-03 14:21:34 +0900

    Added entry detail view

--- resources/urls.py
+++ resources/urls.py
@@ -3,6 +3,7 @@ from .views import (
     HomePageView,
     SearchResultsView,
     EntryCreateView,
+    EntryDetailView,
     EntryUpdateView,
     EntryDeleteView,
     GlossaryUploadView,
@@ -26,6 +27,7 @@ urlpatterns = [
     path('search/', SearchResultsView.as_view(), name='search_results'),
 
     path('entry/new/', EntryCreateView.as_view(), name='entry_create'),
+    path('entry/<int:pk>/detail/', EntryDetailView.as_view(), name='entry_detail'),
     path('entry/<int:pk>/edit/', EntryUpdateView.as_view(), name='entry_update'),
     path('entry/<int:pk>/delete/', EntryDeleteView.as_view(), name='entry_delete'),
 
